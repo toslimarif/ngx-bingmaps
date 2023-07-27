15d1de68a0ed425a42845d1ffff8d513c5a7c395
Toslim Arif <toslim_arif@yahoo.com>
2023-07-28 04:46:02 +0530

    implement service method to load the map from bing sdk

--- projects/ngx-bingmaps/src/lib/ngx-bingmaps.service.ts
+++ projects/ngx-bingmaps/src/lib/ngx-bingmaps.service.ts
@@ -1,9 +1,37 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NgxBingmapsService {
+  private promise: any;
+  private window: Window | null;
+  constructor(@Inject(DOCUMENT) private _documentRef: Document) {
+    this.window = this._documentRef.defaultView;
+  }
 
-  constructor() { }
+  public load() {
+    // First time 'load' is called?
+    if (!this.promise) {
+      // Make promise to load
+      this.promise = new Promise((resolve) => {
+        // Set callback for when bing maps is loaded.
+        // @ts-ignore
+        window['__onBingLoaded'] = () => {
+          resolve('Bing Maps API loaded');
+        };
+        const node = this._documentRef.createElement('script');
+        node.src =
+          'https://www.bing.com/api/maps/mapcontrol?callback=__onBingLoaded&branch=release';
+        node.type = 'text/javascript';
+        node.async = true;
+        node.defer = true;
+        // _documentRef.getElementsByTagName('head')[0].appendChild(node);
+        this._documentRef.getElementsByTagName('head')[0].appendChild(node);
+      });
+    }
+    // Always return promise. When 'load' is called many times, the promise is already resolved.
+    return this.promise;
+  }
 }
