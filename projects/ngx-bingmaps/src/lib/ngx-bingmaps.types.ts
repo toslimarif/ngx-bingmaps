0bbbaa06c1f7d0b3f5d1264ea6091b96e3baba5d
Toslim Arif <toslim_arif@yahoo.com>
2023-07-30 01:10:12 +0530

    add: types for mapTypes and navigationBarMode enums

---
+++ projects/ngx-bingmaps/src/lib/ngx-bingmaps.types.ts
@@ -0,0 +1,18 @@
+export enum MapTypes {
+  aerial = 'aerial',
+  canvasDark = 'canvasDark',
+  canvasLight = 'canvasLight',
+  birdseye = 'birdseye',
+  grayscale = 'grayscale',
+  mercator = 'mercator',
+  ordnanceSurvey = 'ordnanceSurvey',
+  road = 'road',
+  streetside = 'streetside',
+}
+
+export enum NavigationBarModes {
+  compact = 'compact',
+  default = 'default',
+  minified = 'minified',
+  square = 'square',
+}
