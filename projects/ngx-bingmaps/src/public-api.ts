36980daae3455d4d678aa13e1d1ef26498ae3198
Toslim Arif <toslim_arif@yahoo.com>
2023-07-30 01:10:48 +0530

    update: exposing the enum types as well

--- projects/ngx-bingmaps/src/public-api.ts
+++ projects/ngx-bingmaps/src/public-api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of ngx-bingmaps
  */
 
+export * from './lib/ngx-bingmaps.types';
 export * from './lib/ngx-bingmaps.models';
 export * from './lib/ngx-bingmaps.service';
 export * from './lib/ngx-bingmaps.component';
