82f23b310d946dc4a0fa9f38f692c4d160d4170e
Toslim Arif <toslim_arif@yahoo.com>
2023-07-31 05:34:14 +0530

    completeing the demo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MapTypes, NavigationBarModes, PushpinModel } from 'ngx-bingmaps';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +8,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  geoLoc = {
-    latitude: 12.971599,
-    longitude: 77.594566,
-  };
-  apiKey = '<YOUR_API_KEYS_GOES_HERE>';
+  protected readonly window = window;
+
+  apiKey: string;
+  mapType: keyof typeof MapTypes;
+  navigationBarMode: keyof typeof NavigationBarModes;
+  mapTypes: string[] = Object.keys(MapTypes);
+  navigationBarModes: string[] = Object.keys(NavigationBarModes);
+  pushpins: PushpinModel[];
+
+  configForm: FormGroup;
+  pushpinsForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.apiKey = '<YOUR_API_KEY_HERE>';
+    this.mapType = 'road';
+    this.navigationBarMode = 'square';
+    this.pushpins = [];
+    this.configForm = this.formBuilder.group({
+      mapType: [this.mapType],
+      navigationBarMode: [this.navigationBarMode],
+    });
+    this.pushpinsForm = this.formBuilder.group({
+      location: this.formBuilder.group({
+        latitude: [null, Validators.required],
+        longitude: [null, Validators.required],
+      }),
+      options: this.formBuilder.group({
+        text: [null],
+        title: [null],
+        subTitle: [null],
+      }),
+    });
+  }
+
+  onConfigChange() {
+    if (!this.configForm.valid) return;
+    const { mapType, navigationBarMode } = this.configForm.value;
+    this.mapType = mapType;
+    this.navigationBarMode = navigationBarMode;
+  }
+
+  onAddPushpin() {
+    if (!this.pushpinsForm.valid) return;
+    this.pushpins = [...this.pushpins, this.pushpinsForm.value];
+    this.pushpinsForm.reset();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgxBingmapsModule } from 'ngx-bingmaps';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { ToolbarModule } from 'primeng/toolbar';
+import { AccordionModule } from 'primeng/accordion';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+import { NgOptimizedImage } from '@angular/common';
+import { FieldsetModule } from 'primeng/fieldset';
+import { ChipModule } from 'primeng/chip';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NgxBingmapsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgxBingmapsModule,
+    ToolbarModule,
+    ButtonModule,
+    InputTextModule,
+    AccordionModule,
+    ReactiveFormsModule,
+    DropdownModule,
+    NgOptimizedImage,
+    FieldsetModule,
+    ChipModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
